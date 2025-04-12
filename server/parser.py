99ff9226f531cbb26f00e35ae42a760a4eed8519
yuki-oka <55942183+fate7bardiche@users.noreply.github.com>
2025-04-13 01:36:21 +0900

    feat: サーバー側にTCPを使ってチャットルームの作成、及び参加できる機能の実装

---
+++ server/parser.py
@@ -0,0 +1,16 @@
+def parse_protocol(data: bytes):
+    header = data[:32]
+    body = data[32:]
+    return header, body
+
+def parse_protocol_header(header: bytes):
+    room_name_length = int.from_bytes(header[:1], "big")
+    operation = int.from_bytes(header[1:2], "big")
+    state = int.from_bytes(header[2:3], "big")
+    payload_length = int.from_bytes(header[3:33], "big")
+    return room_name_length, operation, state, payload_length
+
+def parse_protocol_body(body: bytes, room_name_length: int, payload_length: int):
+    room_name = body[:room_name_length].decode()
+    operation_payload = body[room_name_length:].decode()
+    return room_name, operation_payload
--- server/server.py
+++ server/server.py
@@ -2,6 +2,14 @@ import socket
 from datetime import datetime, timedelta
 import time
 import threading
+import uuid
+import time
+from parser import parse_protocol, parse_protocol_header, parse_protocol_body
+
+chat_room_list = {}
+client_list = []
+
+hoge = ["sdf", "sdf"]
 
 # 各クライアントの最後のメッセージ送信時刻を追跡し、条件を満たせば削除する
 def remove_client(client_list):
@@ -24,14 +32,12 @@ def remove_client(client_list):
         monitor_wait_second = 5
         time.sleep(monitor_wait_second)
 
-def main():
+def main_udp(client_list: list):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_address = '0.0.0.0'
-    server_port = 9001
+    server_port = 9002
 
     sock.bind((server_address, server_port))
-
-    client_list = []
 
     thread = threading.Thread(target=remove_client, args=(client_list, ), daemon=True)
     thread.start()
@@ -60,5 +66,143 @@ def main():
             print("send to", client_addr[1])
             sock.sendto(data, client_addr)
 
+
+
+
+
+
+
+# TCP関係
+def tcp_flow(connection: socket.socket, address: tuple[str, int]):
+    while True:
+        print("in while")
+
+        try:
+            data = connection.recv(4096)
+        except socket.error as err:
+            print(err)
+            break
+
+        header, body = parse_protocol(data)
+        room_name_length, operation, state, payload_length = parse_protocol_header(header)
+        room_name_bits = body[:room_name_length]
+        room_name, payload = parse_protocol_body(body, room_name_length, payload_length)
+
+        print(room_name_length)
+        print(operation)
+        print(state)
+        print(payload_length)
+        print(room_name)
+        print(payload)
+
+        state += 1
+        status_code = "200"
+        status_code_bits = status_code.encode()
+        state_1_header = create_tcp_header(room_name_length, operation, state, len(status_code_bits))
+        state_1_response = state_1_header + room_name_bits + status_code_bits
+        connection.send(state_1_response)
+
+        time.sleep(0.5)
+
+        # ChatRoomの作成
+        if operation == 1:
+            state += 1
+            host_user_token_uuid = uuid.uuid4()
+            host_user_token = str(host_user_token_uuid)
+
+            # crate chat room
+            host_user = create_user(payload, address, host_user_token)
+            chat_room = {
+                "host_token": host_user_token,
+                "name": room_name,
+                "users": {host_user_token:  host_user}
+            }
+            chat_room_list.setdefault(room_name, chat_room)
+
+            host_user_token_bits = host_user_token.encode()
+            state_2_header = create_tcp_header(room_name_length, operation, state, len(host_user_token_bits))
+            state_2_response = state_2_header + room_name_bits + host_user_token_bits
+            connection.send(state_2_response)
+            print(f"{room_name} チャットルームを作成しました。")
+
+        # ChatRoomに参加
+        elif operation == 2:
+            state += 1
+
+            user_token_uuid = uuid.uuid4()
+            user_token = str(user_token_uuid)
+            user =  host_user = create_user(payload, address, user_token)
+
+            chat_room = chat_room_list[room_name]
+            # todo: チャットルームがないなら、エラーをレスポンスとして返す処理を書く
+            chat_room["users"][user_token] = user
+
+            user_token_bits = user_token.encode()
+            state_2_header = create_tcp_header(room_name_length, operation, state, len(user_token_bits))
+            state_2_response = state_2_header + room_name_bits + user_token_bits
+            connection.send(state_2_response)
+            print(f"{room_name} チャットルームに参加しました。")
+        else:
+            print("operationが不正です。")
+
+        # break
+    
+    # connection.close()
+
+def create_user(user_name: str, address: tuple[str, int], user_token: str):
+    user = {
+            "token": user_token,
+            "user_name": user_name,
+            "ip_address": address,
+            "last_accessed_at": datetime.now()
+        }
+    return user
+
+
+def create_tcp_header(room_name_length: int, opr: int, state_length: int, payload_length: int):
+    return room_name_length.to_bytes(1, "big") + opr.to_bytes(1, "big") + state_length.to_bytes(1, "big") + payload_length.to_bytes(29, "big")
+
+def tcp_main():
+    tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    tcp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server_address = '0.0.0.0'
+    server_port = 9001
+    tcp_sock.bind((server_address, server_port))
+    tcp_sock.listen(5)
+
+    while True:
+        print("before acept")
+        try:
+            connection, client_address = tcp_sock.accept()
+        except socket.error as err:
+            print("err block")
+            print(err)
+            break
+    
+        print("after acept")
+        print(client_address)
+        # tcp_flow(tcp_sock, connection)
+
+        tcp_thread = threading.Thread(target=tcp_flow, args=(connection, client_address), daemon=True)
+        tcp_thread.start()
+        # tcp_thread.join()
+        
+
+
+
+
+        
+
 if __name__ == "__main__":
-    main()
+    
+    # thread_remove_user = threading.Thread(target=remove_client, args=(client_list, ), daemon=True)
+    # thread_tcp_flow = threading.Thread(target=tcp_flow, args=(), daemon=True)
+
+    # thread_remove_user.start()
+    # thread_tcp_flow.start()
+    # tcp_flow()
+    print("tcp finish")
+    # main(client_list)
+    tcp_main()

10d5360410ae794f9e4e5c94c17a9f9932a0bf28
yuki-oka <55942183+fate7bardiche@users.noreply.github.com>
2025-04-15 01:34:46 +0900

    feat: クライアント側にUDPでチャットができる機能を実装

--- client/client.py
+++ client/client.py
@@ -2,39 +2,20 @@ import socket
 import sys
 import threading
 
+token = ""
+user_name = ""
+chat_room_name = ""
 
 # UDP
-def protocol_header(username_length):
-    return int.to_bytes(username_length, 1, "big")
-
-def print_message(bytes_data: bytes):
-    user_name_len = int.from_bytes(bytes_data[:1], "big")
-    user_name = bytes_data[1:user_name_len + 1].decode()
-    message = bytes_data[user_name_len + 1:].decode()
-    print(f"{user_name}: {message}")
-
-def input_message(sock: socket, user_name: str):
-    while True:
-        message = input(f'{user_name} : > ')
-        print("\033[1A\033[2K", end="")
-        print(f"{user_name} : {message}")
-
-        sock.send(protocol_header(len(user_name)) + (user_name + message).encode())
-        
-        socket.timeout(2)
-
-def receive_message(sock: socket):
-    while True:
-        response_data = sock.recv(4096)
-        print("\033[2K\r", end="")
-        print_message(response_data)
-
-
-def main():
+def udp_flow():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     server_address  = "0.0.0.0"
     server_port = 9002
+
+    print("token is ", token)
+    print("user_name is ", user_name)
+    print("chat_room_name is ", chat_room_name)
 
     try:
         sock.connect((server_address, server_port))
@@ -42,20 +23,90 @@ def main():
         print(err)
         sys.exit(1)
 
-    print("接続しました。")
+    # チャットルームに初回参加(作成し参加、　参加のいずれかを問わず)した際のメッセージ送信
+    # 一度メッセージを送信することで、サーバー側にudp_のaddressが保存される
+    first_join_request_message = f'{user_name} : {chat_room_name} に参加しました。'
+    print(first_join_request_message)
+    first_join_request_data = create_udp_protocol(first_join_request_message)
+    sock.send(first_join_request_data)
 
-    user_name = input('ユーザーネームを入力してください。')
+    # sock.send(protocol_header(len(user_name)) + (user_name).encode())
 
-    print(f"{user_name}がチャットに参加しました。")
-
-    sock.send(protocol_header(len(user_name)) + (user_name).encode())
-
-    input_message_thread =  threading.Thread(target=input_message, args=(sock, user_name, ), daemon=True)
+    input_message_thread =  threading.Thread(target=input_message, args=(sock, ), daemon=True)
     receive_message_thread =  threading.Thread(target=receive_message, args=(sock, ), daemon=True)
     input_message_thread.start()
     receive_message_thread.start()
     input_message_thread.join()
     receive_message_thread.join()
+
+# def print_message(bytes_data: bytes):
+#     user_name_len = int.from_bytes(bytes_data[:1], "big")
+#     user_name = bytes_data[1:user_name_len + 1].decode()
+#     message = bytes_data[user_name_len + 1:].decode()
+#     print(f"{user_name}: {message}")
+
+def input_message(sock: socket):
+    while True:
+        message = input(f'{user_name} : > ')
+        print("\033[1A\033[2K", end="")
+        request_message = f"{user_name} : {message}"
+        print(f"{user_name} : {message}")
+
+        request_data = create_udp_protocol(f"{user_name} : {message}")
+
+        sock.send(request_data)
+        
+        socket.timeout(2)
+
+def receive_message(sock: socket.socket):
+    while True:
+        # クライアントが受信するデータにはヘッダーは存在しない。
+        # 送信時のデータのヘッダーが2バイトで、その2を引いた4094をクライアントは受信する。
+        response_data = sock.recv(4094)
+
+        response_message = response_data.decode()
+
+        # header, body = parse_udp_protocol(response_data)
+        # chat_room_name_length, token_length = parse_udp_protocol_header(header)
+
+        print("\033[2K\r", end="")
+        print(response_message)
+        # print_message(response_message)
+
+def create_udp_protocol(message: str):
+    chat_room_name_bits = chat_room_name.encode()
+    token_bits = token.encode()
+    header = create_udp_header(len(chat_room_name_bits), len(token_bits))
+    body = chat_room_name_bits + token_bits + message.encode()
+    return header + body
+
+def create_udp_header(room_name_length: int, token_length: int):
+    return room_name_length.to_bytes(1, "big") + token_length.to_bytes(1, "big")
+
+# def parse_udp_protocol(data: bytes):
+#     header = data[:2]
+#     body = data[2:]
+#     return header, body
+
+# def parse_udp_protocol_header(header: bytes):
+#     chat_room_name_length = int.from_bytes(header[:1], "big")
+#     token_length = int.from_bytes(header[1:], "big")
+#     return chat_room_name_length, token_length
+
+# def parse_udp_protocol_body(body: bytes, chat_room_name_length: int, token_length: int):
+#     response_chat_room_name = body[:chat_room_name_length].decode()
+#     response_token = body[chat_room_name_length:token_length].decode()
+#     message = body[token_length:].decode()
+#     return response_chat_room_name, response_token, message
+
+
+
+
+
+
+
+
+
 
 
 # TCP
@@ -80,26 +131,32 @@ def tcp_flow():
     tcp_sock.settimeout(2)
 
     while True:
+        global chat_room_name
+        global user_name
+
         res_header, res_body = parse_protocol(tcp_sock.recv(4096))
         room_name_length, operation, state, payload_length = parse_protocol_header(res_header)
-        room_name, payload = parse_protocol_body(res_body, room_name_length, payload_length)
+        chat_room_name, payload = parse_protocol_body(res_body, room_name_length, payload_length)
+
         if state == 1:
             print(f"ステータスコードは{payload}です。")
         if state == 2:
-            print("トークンを受け取りました。")
-            print(f"{room_name} チャットルームに参加しました。")
+            print(f"{payload} トークンを受け取りました。")
+            global token
+            token = payload
+            chat_room_name = chat_room_name
+            print(f"{chat_room_name} チャットルームに参加しました。")
             break
-    # tcp_sock.close()
-    input_flow()
+    tcp_sock.close()
 
 
 def input_flow():
-    room_name = input("チャットルーム名を入力してください")
-    user_name = input("ユーザー名を入力してください")
-    operation = input("オペレーションの数字を入力してください。 作成→1, 参加→2")
+    global user_name
 
-    # tcp_sock.send(toriaezu.encode())
-    # operation = 1
+    room_name = input("チャットルーム名を入力してください > ")
+    user_name = input("ユーザー名を入力してください > ")
+    operation = input("オペレーションの数字を入力してください。 作成: 1, 参加: 2  >")
+
     state = 0
     header = create_tcp_header(len(room_name.encode()), int(operation), state, len(user_name.encode()))
     body = room_name.encode() + user_name.encode()
@@ -131,4 +188,8 @@ def create_tcp_header(room_name_length: int, opr: int, state_length: int, payloa
 if __name__ == "__main__":
     # main()
     tcp_flow()
+    udp_flow()
     print("finish!!!!")
+
+
+    # TCPかUDP、どちらのフローを表示するのかに、stateでSwitch的に切り替えるパターン

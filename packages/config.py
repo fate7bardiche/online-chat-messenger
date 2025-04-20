9c1f275313a59ef31892b00e5ca2930ea7fd8235
yuki-oka <55942183+fate7bardiche@users.noreply.github.com>
2025-04-21 01:09:59 +0900

    config: configにフラグ変数を移動 & エラーフラグを追加

--- packages/config.py
+++ packages/config.py
@@ -1,5 +1,11 @@
+# TCP関係
 tcp_server_address = '0.0.0.0'
 tcp_server_port = 9001
 
+# UDP関係
 udp_server_address = '0.0.0.0'
 udp_server_port = 9002
+
+# フラグ関係
+exit_chat_room_flag_str = "exit:"
+error_flag_str = "error:"
